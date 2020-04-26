c513fd2f7e193b626b31fbb4942de0803dcee194
tr07.wfsd24.ene19 <tr07.wfsd24.ene19@bootcamp-upgrade.com>
2020-04-26 17:50:50 +0200

    add section presupuesto

--- js/main.js
+++ js/main.js
@@ -100,16 +100,18 @@ $(document).ready(function(){
     $("a").on('click', function(event) {
 
         // Make sure this.hash has a value before overriding default behavior
-        var href = $(this).attr("href")
-        if ( href.indexOf("index") != -1  ) {
-            setTimeout( function() { scroll(0,0); }, 1000)
-            setTimeout(function(){
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top
-                }, 800, function(){
-                    window.location.hash = hash;
-                });
-            }, 1000);
-        }
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            // Store hash
+            var hash = this.hash;
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
     });
 });
